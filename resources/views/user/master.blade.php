5368002d6bc3717fc3aa2a15894991fd25a83ed6
Tasnimul Karim Ayon <ayon8295@gmail.com>
2021-09-21 14:21:18 +0600

    css, js cdn-fied

--- resources/views/user/master.blade.php
+++ resources/views/user/master.blade.php
@@ -12,7 +12,6 @@
         <title>@yield('page_title')</title>
 
         <!-- Bootstrap Core CSS -->
-        {{-- <link rel="stylesheet" href="{{ asset('user/assets/css/bootstrap.min.css') }}"> --}}
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
             integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
@@ -21,37 +20,33 @@
         <link rel="stylesheet" href="{{ asset('user/assets/css/main.css') }}">
         <link rel="stylesheet" href="{{ asset('user/assets/css/blue.css') }}">
 
-        {{-- <link rel="stylesheet" href="{{ asset('user/assets/css/owl.carousel.css') }}"> --}}
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.css"
             integrity="sha512-RWhcC19d8A3vE7kpXq6Ze4GcPfGe3DQWuenhXAbcGiZOaqGojLtWwit1eeM9jLGHFv8hnwpX3blJKGjTsf2HxQ=="
             crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-        {{-- <link rel="stylesheet" href="{{ asset('user/assets/css/owl.transitions.css') }}"> --}}
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.transitions.css"
             integrity="sha512-4ioNqjewIy2hSnYs7smFWpvzAB4xcD6NnR2z6ydUZEBg0UDVW3IoKATPoVYMyzKexe8yFU6sPd2VypoH2ZjCTQ=="
             crossorigin="anonymous" referrerpolicy="no-referrer" />
 
         {{-- <link rel="stylesheet" href="{{ asset('user/assets/css/animate.min.css') }}"> --}}
-        {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
             integrity="sha512-c42qTSw/wPZ3/5LBzD+Bw5f7bSF2oxou6wEb+I/lqeaKV5FDIfMvvRp772y4jcJLKuGUOpbJMdg/BTl50fJYAw=="
-            crossorigin="anonymous" referrerpolicy="no-referrer" /> --}}
+            crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-        {{-- <link rel="stylesheet" href="{{ asset('user/assets/css/rateit.css') }}"> --}}
-        {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery.rateit/1.1.5/rateit.css"
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery.rateit/1.1.5/rateit.css"
             integrity="sha512-UBZiEmhvSN+QLmroxtPWf4f1SsvHlDTAKqMfRw8g9vxALzXsPd/pkwgULarTBp28QRW+L70bAU6Z5Noh0A+OIg=="
-            crossorigin="anonymous" referrerpolicy="no-referrer" /> --}}
+            crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-        {{-- <link rel="stylesheet" href="{{ asset('user/assets/css/bootstrap-select.min.css') }}"> --}}
-        {{-- <link rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.11.2/css/bootstrap-select.min.css"
-            integrity="sha512-ZACc4FM2d+Iq2BskSukuyDBB9Y2Ewji0/aRTqThaf3Op5u0MkdL7EqEgE6hnjjbP/Igl0bJCWLlGiNIIfr0Cdg=="
-            crossorigin="anonymous" referrerpolicy="no-referrer" /> --}}
+        <link rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.18/css/bootstrap-select.min.css"
+            integrity="sha512-ARJR74swou2y0Q2V9k0GbzQ/5vJ2RBSoCWokg4zkfM29Fb3vZEQyv0iWBMW/yvKgyHSR/7D64pFMmU8nYmbRkg=="
+            crossorigin="anonymous" referrerpolicy="no-referrer" />
 
         <!-- Icons/Glyphs -->
-        {{-- <link rel="stylesheet" href="{{ asset('user/assets/css/font-awesome.css') }}"> --}}
-        {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/fontawesome.min.css"
+        <link rel="stylesheet" href="{{ asset('user/assets/css/font-awesome.css') }}">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/fontawesome.min.css"
             integrity="sha512-P9vJUXK+LyvAzj8otTOKzdfF1F3UYVl13+F8Fof8/2QNb8Twd6Vb+VD52I7+87tex9UXxnzPgWA3rH96RExA7A=="
-            crossorigin="anonymous" referrerpolicy="no-referrer" /> --}}
+            crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 
         {{-- toastr js css --}}
@@ -99,16 +94,43 @@
 
 
         <!-- JavaScripts  -->
-        <script src="{{ asset('user/assets/js/jquery-1.11.1.min.js') }}"></script>
-        <script src="{{ asset('user/assets/js/bootstrap.min.js') }}"></script>
-        <script src="{{ asset('user/assets/js/bootstrap-hover-dropdown.min.js') }}"></script>
-        <script src="{{ asset('user/assets/js/owl.carousel.min.js') }}"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+            integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous">
+        </script>
+
+        <script
+            src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-hover-dropdown/2.2.1/bootstrap-hover-dropdown.min.js"
+            integrity="sha512-bkRnY+Yd8OOKaLeSQ4ywl+eeJKIbJ5TtBvyWwM2OnsV1qeIZb2yi7E4h2P6XVcAMz3ldrTKAXk/lC5vvZnDkZw=="
+            crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.js"
+            integrity="sha512-9CWGXFSJ+/X0LWzSRCZFsOPhSfm6jbnL+Mpqo0o8Ke2SYr8rCTqb4/wGm+9n13HtDE1NQpAEOrMecDZw4FXQGg=="
+            crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
         <script src="{{ asset('user/assets/js/echo.min.js') }}"></script>
-        <script src="{{ asset('user/assets/js/jquery.easing-1.3.min.js') }}"></script>
-        <script src="{{ asset('user/assets/js/bootstrap-slider.min.js') }}"></script>
-        <script src="{{ asset('user/assets/js/jquery.rateit.min.js') }}"></script>
-        <script src="{{ asset('user/assets/js/bootstrap-select.min.js') }}"></script>
-        <script src="{{ asset('user/assets/js/wow.min.js') }}"></script>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"
+            integrity="sha512-0QbL0ph8Tc8g5bLhfVzSqxe9GERORsKhIn1IrpxDAgUsbBGz/V7iSav2zzW325XGd1OMLdL4UiqRJj702IeqnQ=="
+            crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-slider/11.0.2/bootstrap-slider.min.js"
+            integrity="sha512-f0VlzJbcEB6KiW8ZVtL+5HWPDyW1+nJEjguZ5IVnSQkvZbwBt2RfCBY0CBO1PsMAqxxrG4Di6TfsCPP3ZRwKpA=="
+            crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.rateit/1.1.5/jquery.rateit.min.js"
+            integrity="sha512-ttBgr7TNuS+00BFNY+TkWU9chna3buySaRKoA9IMmk+ueesPbUfyEsWdn5mrXB+cG+ziRdEXMHmsJjGmzBZJYQ=="
+            crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.18/js/bootstrap-select.min.js"
+            integrity="sha512-yDlE7vpGDP7o2eftkCiPZ+yuUyEcaBwoJoIhdXv71KZWugFqEphIS3PU60lEkFaz8RxaVsMpSvQxMBaKVwA5xg=="
+            crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"
+            integrity="sha512-Eak/29OTpb36LLo2r47IpVzPBLXnAMPAVypbSZiZ4Qkf8p/7S/XRG5xp7OKWPPYfJT6metI+IORkR5G8F900+g=="
+            crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
         <script src="{{ asset('user/assets/js/scripts.js') }}"></script>
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js">
         </script>
